951283ea7ddf25530ee46a50579eb03ab196215a
nyanrus <68762426+nyanrus@users.noreply.github.com>
2023-07-19 18:34:50 +0900

    Add js-cache and Vite

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
+import Cookies from "./node_modules/js-cookie/dist/js.cookie.min.mjs";
+
+function getDispNamesLang(lang) {
+  return new Intl.DisplayNames([lang], { type: "language" });
+}
+
+const setResult = (jaJson, targetJson) => {
+  const value = document.querySelector("input").value;
+  document.querySelector("div,#result").textContent = `${jaJson[value]} | ${targetJson[value]}`;
+};
+
 const i18n_file_list = await fetch("https://api.github.com/repos/fa0311/TwitterInternalAPIDocument/git/trees/master?recursive=1");
-const i18n_list_json = await i18n_file_list.json();
+const i18n_file_list_json = await i18n_file_list.json();
 
 let arr = [];
 
-console.log(i18n_list_json);
+console.log(i18n_file_list_json);
 
-for (const a of i18n_list_json.tree) {
+for (const a of i18n_file_list_json.tree) {
   if (a.path.startsWith("docs/json/i18n/")) {
     if (!a.path.startsWith("docs/json/i18n/emoji")) {
-      //console.log(a.path.replace("docs/json/i18n/", ""));
       arr.push(a.path.replace("docs/json/i18n/", ""));
     }
   }
 }
 
 console.log(arr);
-const intl_en = new Intl.DisplayNames(["en"], { type: "language" });
+const intl_en = new getDispNamesLang("en");
 
 const sel = document.querySelector("select, #sel_lang");
 for (const a of arr) {
@@ -23,11 +33,10 @@ for (const a of arr) {
   op.value = a;
   const lang = a.replace(".json", "");
   try {
-    op.textContent = `${intl_en.of(lang)} (${new Intl.DisplayNames([lang], { type: "language" }).of(lang)})`;
+    op.textContent = `${intl_en.of(lang)} (${getDispNamesLang(lang).of(lang)})`;
   } catch (err) {
     continue;
   }
-
   sel.appendChild(op);
 }
 
@@ -35,24 +44,21 @@ let jaJson = await (await fetch(`https://raw.githubusercontent.com/fa0311/Twitte
 let targetJson = null;
 
 document.querySelector("select").addEventListener("change", async (e) => {
-  document.cookie = `lang=${e.target.value}`;
+  Cookies.set("lang", e.target.value);
   targetJson = await (await fetch(`https://raw.githubusercontent.com/fa0311/TwitterInternalAPIDocument/master/docs/json/i18n/${e.target.value}`)).json();
-  const value = document.querySelector("input").value;
-  document.querySelector("div,#result").textContent = `${jaJson[value]} | ${targetJson[value]}`;
+  setResult(jaJson, targetJson);
 });
 
 document.querySelector("input").addEventListener("input", async () => {
-  const value = document.querySelector("input").value;
-  document.querySelector("div,#result").textContent = `${jaJson[value]} | ${targetJson[value]}`;
+  setResult(jaJson, targetJson);
 });
 
 if (location.search.startsWith("?search=")) {
   document.querySelector("input").value = location.search.replace("?search=", "");
 }
 
-if (document.cookie.startsWith("lang=")) {
-  document.querySelector("select").value = document.cookie.replace("lang=", "");
+if (Cookies.get("lang") != undefined) {
+  document.querySelector("select").value = Cookies.get("lang");
 }
 targetJson = await (await fetch(`https://raw.githubusercontent.com/fa0311/TwitterInternalAPIDocument/master/docs/json/i18n/${document.querySelector("select,#sel").value}`)).json();
-const value = document.querySelector("input").value;
-document.querySelector("div,#result").textContent = `${jaJson[value]} | ${targetJson[value]}`;
+setResult(jaJson, targetJson);
